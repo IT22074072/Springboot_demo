f6277b47327d695293a27f19e701e0b18ffcc055
IT22074072 <it22074072@my.sliit.lk>
2024-07-06 20:43:10 +0530

    UserRepo interface

--- src/main/java/com/example/demo/repository/UserRepo.java
+++ src/main/java/com/example/demo/repository/UserRepo.java
@@ -1,4 +1,8 @@
 package com.example.demo.repository;
 
-public interface UserRepo {
+
+import com.example.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Integer> {
 }
